8f11ab9c239221c74d10dc31d7f301fac446ab4d
Ángel Julián Bolaño Campos <alu0101139456@ull.edu.es>
2020-11-22 21:04:56 +0000

    Añadidas cabeceras doxygen

--- main.cc
+++ main.cc
@@ -1,3 +1,14 @@
+/**
+ * @file main.cc
+ * @author Ángel Julián Bolaño Campos (alu0101139456@ull.edu.es)
+ * @brief Práctica 8: Autómatas Finitos No Deterministas
+ * @version 0.1
+ * @date 2020-11-22
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
 #include <iostream>
 
 int main() {
--- set.cc
+++ set.cc
@@ -1,4 +1,13 @@
-
+/**
+ * @file set.cc
+ * @author Ángel Julián Bolaño Campos (alu0101139456@ull.edu.es)
+ * @brief Práctica 8: Autómatas Finitos No Deterministas
+ * @version 0.1
+ * @date 2020-11-22
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 
 
 #include <set.h>
